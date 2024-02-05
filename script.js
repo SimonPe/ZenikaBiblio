e7f41e4e8d848915e53b320dd869483dc98cd906
Furio360 <84274074+Furio360@users.noreply.github.com>
2024-02-05 19:33:38 +0100

    0.3

--- script.js
+++
@@ -1,4 +0,0 @@
-function emprunterLivre(titre) {
-    alert('Livre emprunté : ' + titre);
-    // Vous pouvez ajouter ici du code pour gérer l'emprunt du livre dans une base de données, par exemple.
-}
